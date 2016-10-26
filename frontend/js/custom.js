880dcf1a62ec931fa63cc4807083abe8e820a753
Dima Oliynyk <dima@mail.ua>
2016-10-27 00:48:55 +0300

    Mobile version, sandwich style menu, header without background, started work with post page

--- frontend/js/custom.js
+++ frontend/js/custom.js
@@ -1,5 +1,6 @@
-//Fixed header
+
 jQuery(window).load(function(){
+    //Fixed header
 	if (jQuery(this).scrollTop() > 0 && jQuery('body').hasClass('fixed-header')) {
         jQuery('nav.navbar').css({position: 'fixed'});
 	}
@@ -9,7 +10,37 @@ jQuery(window).load(function(){
             jQuery('nav.navbar').css({position: 'fixed'});
         }
         else {
-            jQuery('nav.navbar').css({position: 'static'});
+            if (jQuery('body').hasClass('fixed-header')) {
+                jQuery('nav.navbar').css({position: 'static'});
+            }
         }
+    });
+
+    //Sandwich menu
+    if (jQuery(this).width() < 768) {
+        jQuery('body').addClass('mobile');
+    }
+    jQuery(window).on('resize', function(){
+        if (jQuery(this).width() < 768) {
+            jQuery('body').addClass('mobile');
+        }
+        else {
+            jQuery('body').removeClass('mobile');
+        }
+    });
+
+    jQuery(document).on('click', 'body.mobile #main-menu .menu-item-has-children>a, body.menu-sandwich #main-menu .menu-item-has-children>a', function(){
+        jQuery(this).siblings('ul').slideToggle(600);
+        return false;
+    });
+
+    jQuery(document).on('click', '.navbar-toggle', function(){
+        jQuery(this).fadeOut('fast');
+        jQuery('#main-menu').addClass('open');
+    });
+
+    jQuery(document).on('click', '#main-menu .button-close', function(){
+        jQuery('.navbar-toggle').show();
+        jQuery('#main-menu').removeClass('open');
     });
 });
