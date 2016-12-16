9857a3bbdc5bb0fb8ad846ffe0f53b0e39675c37
Dima Oliynyk <dima@mail.ua>
2016-12-16 11:23:43 +0200

    Fixed Hardcoded attributes in header.php #52

--- backend/visual-composer-starter/header.php
+++ backend/visual-composer-starter/header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
-<html lang="en">
+<html <?php language_attributes(); ?>>
 <head>
-    <meta charset="UTF-8">
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <?php wp_head() ?>
 </head>
