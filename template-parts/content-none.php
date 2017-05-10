78cd7ed9523b131df2041a3c7e72ba5dba90e1f3
edw <edgars.v@wpbakery.com>
2017-05-10 14:34:00 +0300

    Fix missing content-none.php template file #88

---
+++ template-parts/content-none.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * The template part for displaying a message that posts cannot be found
+ *
+ * @package WordPress
+ * @subpackage Twenty_Sixteen
+ * @since Twenty Sixteen 1.0
+ */
+
+?>
+
+<section class="no-results not-found">
+	<header class="page-header">
+		<h1 class="page-title"><?php esc_html_e( 'Nothing Found', 'visual-composer-starter' ); ?></h1>
+	</header><!-- .page-header -->
+
+	<div class="page-content">
+		<?php if ( is_home() && current_user_can( 'publish_posts' ) ) : ?>
+
+			<p><?php
+				/* translators: 1. link opening tag, 2. link closing tag */
+				printf( esc_html__( 'Ready to publish your first post? %1$sGet started here%2$s.', 'visual-composer-starter' ), '<a href="' . esc_url( admin_url( 'post-new.php' ) ) . '">', '</a>' ); ?></p>
+
+		<?php elseif ( is_search() ) : ?>
+
+			<p><?php esc_html_e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'visual-composer-starter' ); ?></p>
+			<?php get_search_form(); ?>
+
+		<?php else : ?>
+
+			<p><?php esc_html_e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'visual-composer-starter' ); ?></p>
+			<?php get_search_form(); ?>
+
+		<?php endif; ?>
+	</div><!-- .page-content -->
+</section><!-- .no-results -->
