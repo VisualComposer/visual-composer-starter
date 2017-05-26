e55f3f17b79705cbfbb14bc448882a7cb5eb0593
edw <edgars.v@wpbakery.com>
2017-05-26 09:51:47 +0300

    Fix footer mailto #149

--- footer.php
+++ footer.php
@@ -96,8 +96,8 @@ if ( vct_is_the_footer_displayed() ) : ?>
 								<?php endif; ?>
 								<?php if ( strlen( get_theme_mod( 'vct_footer_area_social_link_email', '' ) ) ) : ?>
 									<li>
-										<a href="mailto:<?php echo antispambot( get_theme_mod( 'vct_footer_area_social_link_email',
-										                                                   '' ) ); ?>"><span class="vct-icon-mail-circle"></span></a>
+										<a href="mailto:<?php echo esc_html( antispambot( get_theme_mod( 'vct_footer_area_social_link_email',
+										                                                   '' ) ) ); ?>"><span class="vct-icon-mail-circle"></span></a>
 									</li>
 								<?php endif; ?>
 
