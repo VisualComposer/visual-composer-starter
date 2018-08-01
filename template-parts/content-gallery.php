7284adab2ddea70d5f93e5547f95afc9c3472a14
Edgars V <edgars.v@wpbakery.com>
2018-08-01 15:42:20 +0300

    Fix posty type gallery slider, added adaptive height for the slider, added link for post thumbnails in listing pages fixed thumbnails sizes #528223610115152

--- template-parts/content-gallery.php
+++ template-parts/content-gallery.php
@@ -12,15 +12,22 @@
 	<div class="featured-content">
 		<div class="gallery-slider">
 			<?php
-			$gallery = get_post_gallery_images( get_the_ID() );
+			$gallery_images = get_post_gallery( get_the_ID(), false );
+			$gallery_images_ids = explode( ',', $gallery_images['ids'] );
+			$featured_image_width = get_theme_mod( 'vct_overall_site_featured_image_width', 'full_width' );
 
-			foreach ( $gallery as $key => $src ) :
+			foreach ( $gallery_images_ids as $id ) :
+				if ( 'full_width' === $featured_image_width ) {
+					$image = wp_get_attachment_image_src( $id, 'visualcomposerstarter-featured-loop-image-full' );
+				} else {
+					$image = wp_get_attachment_image_src( $id, 'visualcomposerstarter-featured-loop-image-boxed' );
+				}
 				?>
 				<div class="gallery-item">
 					<div class="fade-in-img">
-						<img src="<?php echo esc_url( $src );?>" data-src="<?php echo esc_url( $src );?>">
+						<img src="<?php echo esc_url( $image[0] ); ?>" data-src="<?php echo esc_url( $image[0] ); ?>">
 						<noscript>
-							<img src="<?php echo esc_url( $src );?>">
+							<img src="<?php echo esc_url( $image[0] ); ?>">
 						</noscript>
 					</div><!--.fade-in-img-->
 				</div><!--.gallery-item-->
