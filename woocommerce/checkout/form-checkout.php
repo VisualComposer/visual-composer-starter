5132600b38f75572c0065bce55cad941ee1d82de
Nikita Hlopovs <nikita.h@Nikitas-MacBook-Air.local>
2018-08-17 16:01:21 +0300

    Change markup structure in form-checkout.php

--- woocommerce/checkout/form-checkout.php
+++ woocommerce/checkout/form-checkout.php
@@ -38,14 +38,9 @@ if ( ! $checkout->is_registration_enabled() && $checkout->is_registration_requir
 
 		<?php do_action( 'woocommerce_checkout_before_customer_details' ); ?>
 
-		<div class="col2-set" id="customer_details">
-			<div class="col-1">
-				<?php do_action( 'woocommerce_checkout_billing' ); ?>
-			</div>
-
-			<div class="col-2">
-				<?php do_action( 'woocommerce_checkout_shipping' ); ?>
-			</div>
+		<div class="vct-main-form-content" id="customer_details">
+			<?php do_action( 'woocommerce_checkout_billing' ); ?>
+			<?php do_action( 'woocommerce_checkout_shipping' ); ?>
 		</div>
 
 		<?php do_action( 'woocommerce_checkout_after_customer_details' ); ?>
