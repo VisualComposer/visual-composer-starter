b1cbeb11f50008f3674b54fbbf00b67e45b4250e
Edgars V <edgars.v@wpbakery.com>
2018-08-30 11:15:57 +0300

    Fix codestyle

--- woocommerce/myaccount/my-address.php
+++ woocommerce/myaccount/my-address.php
@@ -17,49 +17,58 @@
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
-    exit; // Exit if accessed directly
+	exit; // Exit if accessed directly.
 }
 
 $customer_id = get_current_user_id();
 
 if ( ! wc_ship_to_billing_address_only() && wc_shipping_enabled() ) {
-    $get_addresses = apply_filters( 'woocommerce_my_account_get_addresses', array(
-        'billing' => __( 'Billing address', 'woocommerce' ),
-        'shipping' => __( 'Shipping address', 'woocommerce' ),
-    ), $customer_id );
+	$get_addresses = apply_filters( 'woocommerce_my_account_get_addresses', array(
+		'billing' => __( 'Billing address', 'woocommerce' ),
+		'shipping' => __( 'Shipping address', 'woocommerce' ),
+	), $customer_id );
 } else {
-    $get_addresses = apply_filters( 'woocommerce_my_account_get_addresses', array(
-        'billing' => __( 'Billing address', 'woocommerce' ),
-    ), $customer_id );
+	$get_addresses = apply_filters( 'woocommerce_my_account_get_addresses', array(
+		'billing' => __( 'Billing address', 'woocommerce' ),
+	), $customer_id );
 }
 
 $oldcol = 1;
-$col    = 1;
+$col = 1;
 ?>
 
-    <p>
-        <?php echo apply_filters( 'woocommerce_my_account_my_address_description', __( 'The following addresses will be used on the checkout page by default.', 'woocommerce' ) ); ?>
-    </p>
+	<p>
+		<?php
+		// @codingStandardsIgnoreLine
+		echo apply_filters( 'woocommerce_my_account_my_address_description', __( 'The following addresses will be used on the checkout page by default.', 'visualcomposer-starter' ) );
+		?>
+	</p>
 
 <?php if ( ! wc_ship_to_billing_address_only() && wc_shipping_enabled() ) : ?>
-    <div class="u-columns woocommerce-Addresses col2-set addresses">
+	<div class="u-columns woocommerce-Addresses col2-set addresses">
 <?php endif; ?>
 
 <?php foreach ( $get_addresses as $name => $title ) : ?>
-
-    <div class="u-column<?php echo ( ( $col = $col * -1 ) < 0 ) ? 1 : 2; ?> col-<?php echo ( ( $oldcol = $oldcol * -1 ) < 0 ) ? 1 : 2; ?> woocommerce-Address">
-        <header class="woocommerce-Address-title title">
-            <h3><?php echo $title; ?></h3>
-        </header>
-        <address><?php
-            $address = wc_get_account_formatted_address( $name );
-            echo $address ? wp_kses_post( $address ) : esc_html_e( 'You have not set up this type of address yet.', 'woocommerce' );
-            ?></address>
-        <a href="<?php echo esc_url( wc_get_endpoint_url( 'edit-address', $name ) ); ?>" class="edit"><?php _e( 'Edit', 'woocommerce' ); ?></a>
-    </div>
+	<?php
+	$col = $col * - 1;
+	$oldcol = $oldcol * - 1
+	?>
+	<div class="u-column<?php echo ( $col < 0 ) ? 1 : 2; ?> col-<?php echo ( $oldcol < 0 ) ? 1 : 2; ?> woocommerce-Address">
+		<header class="woocommerce-Address-title title">
+			<h3><?php echo esc_html( $title ); ?></h3>
+		</header>
+		<address>
+			<?php
+			$address = wc_get_account_formatted_address( $name );
+			// @codingStandardsIgnoreLine
+			echo $address ? wp_kses_post( $address ) : esc_html_e( 'You have not set up this type of address yet.', 'visualcomposer-starter' );
+			?>
+		</address>
+		<a href="<?php echo esc_url( wc_get_endpoint_url( 'edit-address', $name ) ); ?>" class="edit"><?php esc_html_e( 'Edit', 'visualcomposer-starter' ); ?></a>
+	</div>
 
 <?php endforeach; ?>
 
 <?php if ( ! wc_ship_to_billing_address_only() && wc_shipping_enabled() ) : ?>
-    </div>
+	</div>
 <?php endif;
