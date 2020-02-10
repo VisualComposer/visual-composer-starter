b81af870f7229ac5b1bff8c6f4cb85107730b9f3
Edgars V <edgars.v@visualcomposer.com>
2020-02-10 16:01:21 +0200

    Download website builder automatically #265

--- functions.php
+++ functions.php
@@ -1592,6 +1592,12 @@ function visualcomposerstarter_register_required_plugins() {
 		);
 	}
 
+    $plugins[] = array(
+        'name' => 'Visual Composer Website Builder',
+        'slug' => 'visualcomposer',
+        'required' => false,
+    );
+
 	/*
 	 * Array of configuration settings. Amend each line as needed.
 	 */
