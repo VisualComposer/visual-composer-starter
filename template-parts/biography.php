dbf136a5f37128ac17b740bee8a71e1b202f3545
OlegApanovich <dollar4444@gmail.com>
2024-08-27 12:27:00 +0300

    VC-3454 fix issue with author name (#336)
    * VC-3454 fix issue with author name
    
    * VC-3454 rewrite function use in a more consistent way

--- template-parts/biography.php
+++ template-parts/biography.php
@@ -6,7 +6,7 @@
  * @subpackage Visual Composer Starter
  * @since Visual Composer Starter 1.0
  */
-
+$author = get_the_author();
 ?>
 <div class="entry-author-data">
 	<div class="author-avatar">
@@ -17,15 +17,15 @@
 				data-src="<?php echo esc_url( get_avatar_url( get_the_author_meta( 'ID' ), array(
 					'size' => 100,
 				) ) ); ?>"
-				alt="<?php the_author(); ?>">
+				alt="<?php esc_attr_e( $author ); ?>">
 			<noscript>
 				<img src="<?php echo esc_url( get_avatar_url( get_the_author_meta( 'ID' ), array(
 					'size' => 100,
 				) ) ); ?>"
-					alt="<?php the_author(); ?>">
+					alt="<?php esc_attr_e( $author ); ?>">
 			</noscript>
 		</div>
 	</div><!--.author-avatar-->
-	<p class="author-name"><span class="author vcard"><?php the_author(); ?></span></p>
+	<p class="author-name"><span class="author vcard"><?php esc_html_e( $author ); ?></span></p>
 	<p class="author-biography"><?php the_author_meta( 'description' ) ?></p>
 </div><!--.entry-author-data-->
